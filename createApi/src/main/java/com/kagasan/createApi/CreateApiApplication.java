211a05ef52f25f133b69d49a8ce8cfd273c05541
kagasan <kagasan@yandex.ru>
2025-04-11 13:29:18 +0300

    Truly clean start

---
+++ createApi/src/main/java/com/kagasan/createApi/CreateApiApplication.java
@@ -0,0 +1,13 @@
+package com.kagasan.createApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CreateApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CreateApiApplication.class, args);
+	}
+
+}
---
+++ createApi/src/test/java/com/kagasan/createApi/CreateApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kagasan.createApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CreateApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
